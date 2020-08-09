b644c0ae0a1a7418db98ef09571d2bdcfdffa246
Carrie Mathieu <carrieburks@gmail.com>
2020-08-09 20:38:16 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,11 +7,11 @@ def my_all?(collection)
   block_return_values << yield(collection[i])
      i += 1 
   end
-end
-
-if block_return_values.include?(false)
+  if block_return_values.include?(false)
     false
   else
     true
   end
 end
+end
+
